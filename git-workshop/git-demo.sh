d0ee584dfff9fb98cb3e60926b008b5f54fb5b15
Brian Mo <brimo2912@gmail.com>
2017-10-03 10:31:22 -0400

    Git Workshop Demo

--- git-workshop/git-demo.sh
+++ git-workshop/git-demo.sh
@@ -1,10 +1,22 @@
 # Git Commands Demo (Tested on Unix (Mac and Linux))
+
 # Git Best Practices: https://github.com/trein/dev-best-practices/wiki/Git-Commit-Best-Practices
-# A helpful website: http://rogerdudler.github.io/git-guide/
-# Another helpful website: https://www.git-tower.com/blog/git-cheat-sheet/
+# General commands help website: http://rogerdudler.github.io/git-guide/
+# Another commands help website: https://www.git-tower.com/blog/git-cheat-sheet/
+
+#------------------------------
+# Advanced Topics (You don't need to know these topics for this course):
+# Branching: https://git-scm.com/book/en/v2/Git-Branching-Branches-in-a-Nutshell
+# Pull Requests: http://codeinthehole.com/tips/pull-requests-and-other-good-practices-for-teams-using-github/
+# Forking: https://gist.github.com/Chaser324/ce0505fbed06b947d962
+# Pull Requests Best Practices: http://codeinthehole.com/tips/pull-requests-and-other-good-practices-for-teams-using-github/
+#------------------------------
+
 # Delete "#$" from any line containing it to run the command.
 
 
+# BEGIN DEMO
+#==============================
 
 # (General help commands)
    #a.Get help on Git.
@@ -48,7 +60,7 @@
 # Add a file for staging
 #$ git add <file name>
 
-
+# <Sample code>
 
 # Add all files in the current Git repository for staging
 #$ git add .
@@ -92,3 +104,8 @@
 
 # Push your commits to the remote repository
 #$ git push <name of remote repository> <branch>
+
+
+
+# Get the changes from one commit to another
+#$ git diff
