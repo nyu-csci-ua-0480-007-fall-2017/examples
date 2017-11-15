edc12e88884adf472d1f796ee6ae5d465d8bdfed
Radhika Mattoo <rm3485@nyu.edu>
2017-11-14 19:50:50 -0500

    dom workshop

---
+++ dom-workshop/main.js
@@ -0,0 +1,10 @@
+// TODO: Run the JS file only when the HTML page is fully loaded
+
+function main(){
+  // TODO: Change the h1 id to sad and the p class to happy.
+
+  // TODO: Append this h3 element below the paragraph:
+  // <h3 class="angry"> I'm an angry h3 header! </h3>
+
+  // TODO: Add an event listener to h1 that toggles the background color on every click
+}
